6cdcc640b6bd09da0490e45fd9cad5e0559fcb13
Tran Cong Trinh <congtrinh097@gmail.com>
2017-10-19 17:28:32 +0700

    Init Framework for .Net core 2.0
    Init Framework for .Net core 2.0

---
+++ src/Base.Repository/IRepositoryTemplate.cs
@@ -0,0 +1,37 @@
+﻿
+using Base.Entities;
+using HTActive.Core.Repository;
+namespace Base.Repository
+{
+    public partial interface IClaimRepository : IBaseRepository<Claim>
+    {
+    }
+
+    public partial interface IImageRepository : IBaseRepository<Image>
+    {
+    }
+
+    public partial interface IRoleRepository : IBaseRepository<Role>
+    {
+    }
+
+    public partial interface IRoleClaimRepository : IBaseRepository<RoleClaim>
+    {
+    }
+
+    public partial interface IUserRepository : IBaseRepository<User>
+    {
+    }
+
+    public partial interface IUserLoginTokenRepository : IBaseRepository<UserLoginToken>
+    {
+    }
+
+    public partial interface IUserProfileRepository : IBaseRepository<UserProfile>
+    {
+    }
+
+    public partial interface IUserRoleRepository : IBaseRepository<UserRole>
+    {
+    }
+}
---
+++ src/Base.Web/EFCodeFirstHelper.cs
@@ -0,0 +1,33 @@
+﻿using Base.Entities;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Base.Web
+{
+    public class TemporaryDbContextFactory : IDesignTimeDbContextFactory<InstanceEntities>
+    {
+        public InstanceEntities CreateDbContext(string[] args)
+        {
+           
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile($"appsettings.{"Development"}.json", optional: true)
+                .AddEnvironmentVariables();
+                
+            var configuration = builder.Build();
+            var optionBuilder = new DbContextOptionsBuilder<InstanceEntities>();
+            optionBuilder.UseSqlServer(configuration.GetConnectionString("BaseDBConnection"),
+                b => b.MigrationsAssembly("Base.Web")
+                );
+            return new InstanceEntities(optionBuilder.Options);
+        }
+    }
+}
